d5a1e90d2cbf8f8a5eb3386fb954c13666c575b5
Peter Kim <peterckim96@gmail.com>
2018-06-04 15:17:07 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,11 @@
 # Make your shoe class here!
 class Shoe
+  attr_accessor :brand, :color, :size, :material, :contidion
+  
   def initialize(brand)
-    
+    @brand =  brand
+  end
+  
+  
   
 end
